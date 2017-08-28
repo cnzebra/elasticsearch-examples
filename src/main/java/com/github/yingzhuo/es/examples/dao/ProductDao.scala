ec57cc29ca715a554cf578212bce2dc3566ff9df
应卓 <yingzhor@gmail.com>
2017-08-28 09:03:35 +0800

    update

--- src/main/java/com/github/yingzhuo/es/examples/dao/ProductDao.scala
+++ src/main/java/com/github/yingzhuo/es/examples/dao/ProductDao.scala
@@ -11,9 +11,4 @@ package com.github.yingzhuo.es.examples.dao
 import com.github.yingzhuo.es.examples.module.Product
 import org.springframework.data.jpa.repository.JpaRepository
 
-trait ProductDao extends JpaRepository[Product, String] with ProductExtDao {
-}
-
-trait ProductExtDao
-
-class ProductDaoImpl extends ProductExtDao
+trait ProductDao extends JpaRepository[Product, String]
